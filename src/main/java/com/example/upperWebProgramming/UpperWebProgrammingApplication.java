0c911c2b06a97c9c282c19e4fae2278bf29cc0a7
yoobin kim <yoobin0821@naver.com>
2025-05-13 23:12:17 +0900

    기본 파일

---
+++ src/main/java/com/example/upperWebProgramming/UpperWebProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.upperWebProgramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpperWebProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpperWebProgrammingApplication.class, args);
+	}
+
+}
